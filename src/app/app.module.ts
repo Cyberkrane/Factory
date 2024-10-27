2be7f97841d19800ea37992c8d3b4bf1bbed204d
Cyberkrane <cyberkrane@gmail.com>
2024-10-27 13:34:46 -0300

    feat(dashboard): añade Dashboard para mostrar y contener los modulos de inventario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// Modules
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
+import { DashboardModule } from './features/dashboard/dashboard.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
+    BrowserAnimationsModule, 
+    BrowserModule,
     CoreModule,
+    DashboardModule,
     SharedModule
   ],
   providers: [],
---
+++ src/app/features/dashboard/dashboard.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent
+  ],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    SharedModule
+  ],
+  exports: [
+    DashboardComponent
+  ],
+  providers: [],
+})
+export class DashboardModule { }
