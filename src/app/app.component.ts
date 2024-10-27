b1cfbfcbf5db4818999b6c8d9bc3b3f27378869a
Cyberkrane <cyberkrane@gmail.com>
2024-10-27 16:35:51 -0300

    spinner probado y funcioando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { SpinnerStore } from './core/stores/spinner.store.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [SpinnerStore]
 })
 export class AppComponent {
-  title = 'Factory';
+ 
+  public spinnerOnState$ = this.spinnerStore.isLoading$
+
+  constructor(public spinnerStore: SpinnerStore) { }
+
 }
--- src/app/features/dashboard/dashboard-routing.module.ts
+++ src/app/features/dashboard/dashboard-routing.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { InventoryComponent } from '../inventory/inventory/inventory.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: DashboardComponent,
-    children: [
-      {path: '', component: DashboardComponent}
-    ]
+    component: InventoryComponent,
   }
 ];
 
